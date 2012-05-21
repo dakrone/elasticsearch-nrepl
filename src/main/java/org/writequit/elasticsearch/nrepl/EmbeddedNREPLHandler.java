754280b64592bfb5ce924733d112b78b98009aba
Lee Hinman <lee@writequit.org>
2012-05-21 15:48:18 -0700

    start up on a random port, get var binding working correctly

--- src/main/java/org/writequit/elasticsearch/nrepl/EmbeddedNREPLHandler.java
+++ src/main/java/org/writequit/elasticsearch/nrepl/EmbeddedNREPLHandler.java
@@ -1,17 +1,19 @@
 package org.writequit.elasticsearch.nrepl;
 
-import clojure.lang.Keyword;
-import clojure.lang.RT;
-import clojure.lang.Symbol;
+import clojure.lang.*;
 import org.elasticsearch.common.component.AbstractComponent;
 import org.elasticsearch.common.inject.Inject;
 import org.elasticsearch.common.settings.Settings;
+import org.elasticsearch.node.internal.InternalNode;
+
+import java.util.Random;
 
 /**
  * @author dakrone
  */
 public class EmbeddedNREPLHandler extends AbstractComponent {
     private final boolean enabled = true;
+    public final String internalNode;
 
     private boolean alreadyStarted = false;
 
@@ -21,13 +23,19 @@ public class EmbeddedNREPLHandler extends AbstractComponent {
     public EmbeddedNREPLHandler(Settings settings) {
         super(settings);
         this.portKW = (Keyword) RT.var("clojure.core", "keyword").invoke("port");
+        // TODO: bind this to a useful object
+        this.internalNode = "this will hopefully be an internalNode someday";
     }
 
     public void startupNREPL() {
+        Random r = new Random(System.currentTimeMillis());
+        int portNum = r.nextInt(1000) + 9400;
+        logger.info("startupNREPL {} {}", this.enabled, this.alreadyStarted);
         if (this.enabled && !this.alreadyStarted) {
-            logger.info("starting nREPL on port 8200");
+            logger.info("starting nREPL on port {}", portNum);
             RT.var("clojure.core", "require").invoke(Symbol.intern("clojure.tools.nrepl.server"));
-            RT.var("clojure.tools.nrepl.server", "start-server").invoke(portKW, 8200);
+            RT.var("clojure.tools.nrepl.server", "start-server").invoke(portKW, portNum);
+            Var.intern(Namespace.findOrCreate(Symbol.intern("user")), Symbol.intern("node"), this.internalNode);
             this.alreadyStarted = true;
         } else {
             logger.info("not starting nREPL, enabled: {}, alreadyStarted: {}", this.enabled, this.alreadyStarted);
